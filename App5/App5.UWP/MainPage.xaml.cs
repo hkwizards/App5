d5d8ff9fc68ea2c1544c5a11e9fd2897442806b0
Paul <Paul@PAUL-MP2007>
2017-10-03 17:05:02 +0800

    first import

---
+++ App5/App5.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace App5.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new App5.App());
+        }
+    }
+}
---
+++ App5/App5/App.xaml.cs
@@ -0,0 +1,69 @@
+﻿using App5.Views;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace App5
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            SetMainPage();
+        }
+
+        public static void SetMainPage()
+        {
+            Current.MainPage = new TabbedPage
+            {
+                Children =
+                {
+                    new NavigationPage(new ItemsPage())
+                    {
+                        Title = "Browse",
+                        Icon = Device.OnPlatform("tab_feed.png",null,null)
+                    },
+                    new NavigationPage(new ItemsPage())
+                    {
+                        Title = "Browse1",
+                        Icon = Device.OnPlatform("tab_feed.png",null,null)
+                    },
+                    new NavigationPage(new ItemsPage())
+                    {
+                        Title = "Browse2",
+                        Icon = Device.OnPlatform("tab_feed.png",null,null)
+                    },
+                    new NavigationPage(new ItemsPage())
+                    {
+                        Title = "Browse3",
+                        Icon = Device.OnPlatform("tab_feed.png",null,null)
+                    },
+                    new NavigationPage(new ItemsPage())
+                    {
+                        Title = "Browse4",
+                        Icon = Device.OnPlatform("tab_feed.png",null,null)
+                    },
+                    new NavigationPage(new ItemsPage())
+                    {
+                        Title = "Browse5",
+                        Icon = Device.OnPlatform("tab_feed.png",null,null)
+                    },
+                    new NavigationPage(new ItemsPage())
+                    {
+                        Title = "Browse6",
+                        Icon = Device.OnPlatform("tab_feed.png",null,null)
+                    },
+
+                    new NavigationPage(new AboutPage())
+                    {
+                        Title = "About",
+                        Icon = Device.OnPlatform("tab_about.png",null,null)
+                    },
+                }
+            };
+        }
+    }
+}
